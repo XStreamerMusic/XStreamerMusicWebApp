4bed00661d5da6b568bab32661798134c24a4e88
Kgosi Mogotsi <rhulanimogotsi@gmail.com>
2025-07-26 20:42:10 -0400

    added templates

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def waitlist(request):
+
+    context = {
+        "page_title": "Join the New Wave"
+    }
+
+    return render(request, 'waitlist.html', context)
