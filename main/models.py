c4316973e54dfb98eac2b4ee217f390c5baefb15
Kgosi Mogotsi <rhulanimogotsi@gmail.com>
2025-07-27 22:25:33 -0400

    modified models and amde migrations

--- main/models.py
+++ main/models.py
@@ -3,4 +3,7 @@ from django.db import models
 # Create your models here.
 class Waitlist(models.Model):
 
-    email = models.EmailField(max_length=100)
+    email = models.EmailField(unique=True, max_length=100)
+
+    def __str__(self):
+        return self.email
