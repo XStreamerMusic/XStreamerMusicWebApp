7a5599ead659e735d62c7d85aac16793eb928820
Kgosi Mogotsi <rhulanimogotsi@gmail.com>
2025-07-27 22:26:07 -0400

    created waitlist submission view

--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
+from django.db import IntegrityError
+from .models import Waitlist
 from .forms import WaitlistForm
 from django.conf import settings
+from django.contrib import messages
 from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
@@ -23,13 +26,26 @@ def join_waitlist(request):
 
             email = form.cleaned_data["email"]
 
-            subject = "Waitlist just got longer!"
-            message = f"There's a new member on the waitlist:\n\n{email}"
-            sender = settings.EMAIL_HOST_USER
-            recipients = ["xstreamermusic@gmail.com"]
+            try:
+                new, created = Waitlist.objects.get_or_create(email=email)
+                
+                if not created:
+                    messages.error(request, "This email is already on the waitlist")
+                    return redirect('waitlist')
+                
+                subject = "Waitlist just got longer!"
+                message = f"There's a new member on the waitlist:\n\n{email}"
+                sender = settings.EMAIL_HOST_USER
+                recipients = ["xstreamermusic@gmail.com"]
 
-            send_mail(subject, message, sender, recipients)
+                send_mail(subject, message, sender, recipients)
 
-            return redirect('waitlist')
+                messages.success(request, "You've been added to the waitlist! See you soon!")
+            except Exception as e:
+                messages.error(request, e)
+            finally:
+                return redirect('waitlist')
+
         else:
-            return HttpResponse("The form was not valid")
+            messages.error(request, e)
+            return redirect('waitlist')
