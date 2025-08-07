62db4a4e42f13b6dd978327e68b86fac3a8056e1
Rhulani Mogotsi <rhulanimogotsi@gmail.com>
2025-08-07 08:49:14 +0200

    whitenoise configuration

--- XStreamerMusicWebApp/settings.py
+++ XStreamerMusicWebApp/settings.py
@@ -144,10 +144,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+# Static files configuration for Vercel
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# For Vercel, use basic WhiteNoise storage
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+# Make sure WhiteNoise can find the files
+WHITENOISE_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- build_files.sh
+++ build_files.sh
@@ -3,7 +3,7 @@ echo "Installing dependencies..."
 pip install -r requirements.txt
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
 
 echo "Running database migrations..."  
 python manage.py migrate
