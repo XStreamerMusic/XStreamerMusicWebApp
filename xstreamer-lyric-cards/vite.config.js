39045587d57055044e18db2be88bdc3e0d4a4c1a
Rhulani Mogotsi <rhulanimogotsi@gmail.com>
2025-09-09 05:40:44 +0200

    added Nav section, dark-mode toggler, and ratio options

--- xstreamer-lyric-cards/vite.config.js
+++ xstreamer-lyric-cards/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,   // required to accept external connections
+    port: 5173,
+    allowedHosts: [
+      '.ngrok-free.app', // wildcard domains allowed
+      'localhost',
+      '127.0.0.1',
+    ],
+  },
 })
