d08294efc9ffc734ad188990d77bb8edf30b00a1
Rhulani Mogotsi <rhulanimogotsi@gmail.com>
2025-09-14 21:46:48 +0200

    updated SITE_URL

--- XStreamerMusicWebApp/settings.py
+++ XStreamerMusicWebApp/settings.py
@@ -150,6 +150,10 @@ USE_I18N = True
 USE_TZ = True
 
 
+# ROUTING
+
+SITE_URL = "xstreamermusic.xyz"
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
