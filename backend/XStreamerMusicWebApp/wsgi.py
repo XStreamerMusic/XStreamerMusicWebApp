66db43b0fa1c1323d67af093a90469cb162f5626
Winter The Engineer <rhulanimogotsi@gmail.com>
2026-04-25 16:53:34 +0200

    minor fixes

--- backend/XStreamerMusicWebApp/wsgi.py
+++ backend/XStreamerMusicWebApp/wsgi.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
 from django.core.management import call_command
 import django
