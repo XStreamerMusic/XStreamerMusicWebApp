123e736344843e034da082c200811defbecf5ca8
Winter The Engineer <rhulanimogotsi@gmail.com>
2026-05-06 13:53:35 +0200

    prerender changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import prerender from "vite-plugin-prerender";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [
+		react(),
+		prerender({
+			staticDir: "dist",
+			routes: [
+				"/",
+				"/waitlist",
+				"/giveaways",
+				"/giveaways/spotify-premium",
+				"/giveaways/apple-music",
+				"/giveaways/youtube-music"
+			]
+		})
+	],
 })
