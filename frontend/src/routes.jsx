d605de369e7f836e6db9bf82d08a45d08b360522
Winter The Engineer <rhulanimogotsi@gmail.com>
2026-08-05 11:39:18 +0200

    re routing

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -2,13 +2,13 @@ import Lyrics from './pages/Lyrics';
 import Giveaways from './pages/Giveaways';
 import Waitlist from './pages/Waitlist';
 import GiveawayDetail from './pages/GiveawayDetail';
-import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom"
+import { Route, createBrowserRouter, createRoutesFromElements, Navigate } from "react-router-dom"
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
         <>
             <Route path="/" element={<Waitlist />} />
-            <Route path="/waitlist" element={<Waitlist />} />
+            <Route path="/waitlist" element={<Navigate to="/" replace />} />
             <Route path="/giveaways" element={<Giveaways />} />
             <Route path="/lyric-cards" element={<Lyrics />} />
             <Route path="/giveaways/:slug" element={<GiveawayDetail />} />
